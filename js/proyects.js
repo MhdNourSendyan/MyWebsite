771332af731b51ed732b17fb2df3c83f9e096a41
Nour._07 <91376968+MhdNourSendyan@users.noreply.github.com>
2022-05-21 15:22:25 +0200

    .

---
+++ js/proyects.js
@@ -0,0 +1,20 @@
+const proyects = [
+    {
+        name: 'Calculadora',
+        description: 'Calculadora básica hecha con java.',
+        githubUrl: 'https://github.com/MhdNourSendyan/Proyectos/tree/main/JAVA/Calculadura',
+        // url: '',
+        imagePath: '../img/calculadora.png'
+    }
+    //,
+    // {
+    //     name: '',
+    //     description: '',
+    //     githubUrl: '',
+    //     url: '',
+    //     imagePath: ''
+    // },
+
+];
+
+export { proyects };
